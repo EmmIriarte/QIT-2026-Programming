e25c57212fd53e4ab990f266ec5f072d0862a929
neet1313 <prabhneet13@gmail.com>
2026-01-22 18:53:11 +0100

    feat: add pages app with home view and template

--- sections/prabhneet/urls.py
+++ sections/prabhneet/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),     # Homepage
-    path('app1/', include('app1.urls')), # app1 routes
-    path('app2/', include('app2.urls')), # app2 routes
+    path('', include('pages/pages.urls')),     # Homepage
+    # path('app1/', include('app1.urls')), # app1 routes
+    # path('app2/', include('app2.urls')), # app2 routes
 ]
